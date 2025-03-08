78ac5e2d70a3c32c0dbd85b5fdd3736828afb27d
Bohdan Ksonov <bogdan.ksenov@gmail.com>
2025-03-08 11:57:03 +0100

    refact/Models: switch models to interfaces instead of classes

--- src/models/ExpenseModel.ts
+++ src/models/ExpenseModel.ts
@@ -3,7 +3,7 @@ export enum ExpenseStatus {
   Excluded = "excluded",
   Failed = "failed",
 }
-interface ExpenseModelOptions {
+export interface ExpenseModel {
   expenseId: string;
   companyId: string;
   reportId: string;
@@ -11,40 +11,17 @@ interface ExpenseModelOptions {
   amount: string;
   image: string;
   status?: ExpenseStatus;
-}
-export class ExpenseModel {
-  _id: string;
-  expenseId: string;
-  companyId: string;
-  reportId: string;
-  companyName: string;
-  amount: string;
-  image: string;
-  status?: ExpenseStatus;
-
-  constructor(options: ExpenseModelOptions) {
-    this._id = options.expenseId;
-    this.expenseId = options.expenseId;
-    this.companyId = options.companyId;
-    this.reportId = options.reportId;
-    this.companyName = options.companyName;
-    this.amount = options.amount;
-    this.image = options.image;
-  }
-
-  static fromArray(expensesData: ExpenseModelOptions[]) {
-    return expensesData.map((e) => new ExpenseModel(e));
-  }
-
-  static toMinified(expense: ExpenseModel | MinifiedExpense): MinifiedExpense {
-    return {
-      _id: expense._id,
-      expenseId: expense.expenseId,
-      amount: expense.amount,
-      image: expense.image,
-    };
-  }
 }
 
 export interface MinifiedExpense
   extends Omit<ExpenseModel, "reportId" | "companyId" | "companyName"> {}
+
+export const minifyExpense = (
+  expense: ExpenseModel | MinifiedExpense
+): MinifiedExpense => {
+  return {
+    expenseId: expense.expenseId,
+    amount: expense.amount,
+    image: expense.image,
+  };
+};
--- src/models/ReportModel.ts
+++ src/models/ReportModel.ts
@@ -1,48 +1,32 @@
 import _ from "lodash";
-import { ExpenseModel, MinifiedExpense } from "./ExpenseModel";
+import { ExpenseModel, MinifiedExpense, minifyExpense } from "./ExpenseModel";
+import { ObjectId } from "mongodb";
 
 interface ReporModelOptions {
+  _id?: string | ObjectId;
   reportId: string;
   companyId: string;
   companyName: string;
   expenses: (ExpenseModel | MinifiedExpense)[];
 }
 
-export class ReportModel {
-  _id: string;
+export interface ReportModel {
   reportId: string;
   companyId: string;
   companyName: string;
-  expenses: (MinifiedExpense | MinifiedExpense)[];
-
-  constructor({
-    reportId,
-    companyId,
-    companyName,
-    expenses,
-  }: ReporModelOptions) {
-    this._id = `${companyId}-${reportId}`; // assume those fields combination is always uniq
-    this.reportId = reportId;
-    this.companyId = companyId;
-    this.companyName = companyName;
-    this.expenses = expenses.map((e) => ExpenseModel.toMinified(e));
-  }
-
-  static fromExpenses(expenses: ExpenseModel[]) {
-    const expensesByReport = _.groupBy(
-      expenses,
-      (e) => e.companyId + e.reportId
-    );
-    const reports: ReportModel[] = Object.values(expensesByReport).map(
-      (expenseGroup) =>
-        new ReportModel({
-          reportId: expenseGroup[0].reportId,
-          companyId: expenseGroup[0].companyId,
-          companyName: expenseGroup[0].companyName,
-          expenses: expenseGroup,
-        })
-    );
-
-    return reports;
-  }
+  expenses: MinifiedExpense[];
 }
+
+export const reportsFromExpenses = (expenses: ExpenseModel[]): ReportModel[] => {
+  const expensesByReport = _.groupBy(expenses, (e) => e.companyId + e.reportId);
+  const reports: ReportModel[] = Object.values(expensesByReport).map(
+    (expenseGroup) => ({
+      reportId: expenseGroup[0].reportId,
+      companyId: expenseGroup[0].companyId,
+      companyName: expenseGroup[0].companyName,
+      expenses: expenseGroup.map((e) => minifyExpense(e)),
+    })
+  );
+
+  return reports;
+};
